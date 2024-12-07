a572516c8cfe1d16dab6c5df17c2b60b58d26bd2
Linusvall <linusvallgren@gmail.com>
2024-12-08 00:18:15 +0100

    Start of PlayerStats
    -Created a struct to hold playerinformation (damage & damage taken)
    - Created a custom player state
    - Changed default player state in gamemodet
    - Cleaned up GM_3v1 blueprint

---
+++ Source/Proj/MythbreakPlayerState.cpp
@@ -0,0 +1,26 @@
+﻿#include "MythbreakPlayerState.h"
+
+#include "Net/UnrealNetwork.h"
+
+AMythbreakPlayerState::AMythbreakPlayerState()
+{
+}
+
+void AMythbreakPlayerState::AddDamageDealt(float Damage)
+{
+	PlayerStats.DamageDealt += Damage;
+}
+
+void AMythbreakPlayerState::AddDamageTaken(float DamageTaken)
+{
+	PlayerStats.DamageTaken += DamageTaken;
+}
+
+void AMythbreakPlayerState::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AMythbreakPlayerState, PlayerStats);
+}
+
+
